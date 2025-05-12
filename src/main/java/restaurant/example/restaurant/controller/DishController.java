58efdc32a1952ea0288d5f17722657c118d4c715
Hoan8386 <nguyenthanhhoan300904@gmail.com>
2025-05-12 16:38:26 +0700

    dish and pagination

--- src/main/java/restaurant/example/restaurant/controller/DishController.java
+++ src/main/java/restaurant/example/restaurant/controller/DishController.java
@@ -6,11 +6,21 @@ import jakarta.validation.Valid;
 import restaurant.example.restaurant.domain.Dish;
 import restaurant.example.restaurant.service.DishService;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DishController {
@@ -21,9 +31,38 @@ public class DishController {
     }
 
     @PostMapping("/dish")
-    public ResponseEntity<Dish> postMethodName(@Valid @RequestBody Dish dish) {
-
+    public ResponseEntity<Dish> createDish(@Valid @RequestBody Dish dish) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.dishService.handleCreatedDish(dish));
     }
 
+    @GetMapping("/dish/{id}")
+    public ResponseEntity<Dish> getDishById(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(this.dishService.handleGetDishById(id).isPresent() ? this.dishService.handleGetDishById(id).get()
+                        : null);
+    }
+
+    @GetMapping("/dish")
+    public ResponseEntity<List<Dish>> getAllDish(
+            @RequestParam("current") Optional<String> currentOptional,
+            @RequestParam("pageSize") Optional<String> pageSizeOptional) {
+        String sCurrent = currentOptional.isPresent() ? currentOptional.get() : "";
+        String sPageSizeOptional = pageSizeOptional.isPresent() ? pageSizeOptional.get() : "";
+
+        int current = Integer.parseInt(sCurrent);
+        int pageSize = Integer.parseInt(sPageSizeOptional);
+        Pageable pageable = PageRequest.of(current - 1, pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(this.dishService.handleGetAllDish(pageable));
+    }
+
+    @PutMapping("/dish")
+    public ResponseEntity<Dish> updateDish(@Valid @RequestBody Dish dish) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.dishService.handleUpdateDish(dish));
+    }
+
+    @DeleteMapping("/dish/{id}")
+    public ResponseEntity<Void> deleteDishById(@PathVariable("id") Long id) {
+        this.dishService.handleDeleteDishById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
--- src/main/java/restaurant/example/restaurant/service/DishService.java
+++ src/main/java/restaurant/example/restaurant/service/DishService.java
@@ -1,5 +1,9 @@
 package restaurant.example.restaurant.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import restaurant.example.restaurant.domain.Dish;
@@ -15,5 +19,21 @@ public class DishService {
 
     public Dish handleCreatedDish(Dish dish) {
         return this.dishRepository.save(dish);
+    }
+
+    public Optional<Dish> handleGetDishById(Long id) {
+        return this.dishRepository.findById(id);
+    }
+
+    public List<Dish> handleGetAllDish(Pageable pageable) {
+        return this.dishRepository.findAll(pageable).getContent();
+    }
+
+    public Dish handleUpdateDish(Dish dish) {
+        return this.dishRepository.save(dish);
+    }
+
+    public void handleDeleteDishById(Long id) {
+        this.dishRepository.deleteById(id);
     }
 }
